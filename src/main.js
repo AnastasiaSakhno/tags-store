f2ea768260371c0a49454381e5223c5cf9aa8248
Anastasiia Sakhno <ask@anahoret.com>
2017-09-02 20:39:55 +0300

    rendered link box wrapped into provider hoc

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import middlewares from './middlewares'
+import reducers from './reducers'
+import LinkBox from './components/LinkBox'
+
+const store = createStore(reducers, middlewares)
 
 ReactDOM.render(
-  <div>Future links&tags store</div>,
+  <Provider store={ store }>
+    <LinkBox/>
+  </Provider>,
   document.getElementById('root')
 )
