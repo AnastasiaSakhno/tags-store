a3ff6627317b5e5e57607af1a177f7702f8d158d
Anastasiia Sakhno <ask@anahoret.com>
2017-09-02 20:37:06 +0300

    added link component

---
+++ src/components/Link.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Link = ({ url }) => {
+  return (
+    <div className="link">
+      { url }
+    </div>
+  )
+}
+
+Link.propTypes = {
+  link: PropTypes.string
+}
+
+export default Link
