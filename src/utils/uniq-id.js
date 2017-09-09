e91aaab2918ee4e22c378eb02ab2ec25ff89d991
Anastasiia Sakhno <ask@anahoret.com>
2017-09-09 20:35:25 +0300

    added custom uniq id method

---
+++ src/utils/uniq-id.js
@@ -0,0 +1,20 @@
+const times = (x) => (f) => {
+  if (x > 0) {
+    f()
+    times (x - 1) (f)
+  }
+}
+
+const s4 = () => {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1)
+}
+
+const uniqId = () => {
+  let id = ''
+  times(4) ( () => { id += s4() })
+  return id
+}
+
+export default uniqId
