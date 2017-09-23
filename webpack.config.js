517d9a34d013c02d471bdf17b46bc5ff107a5d3a
Anastasiia Sakhno <ask@anahoret.com>
2017-09-23 18:25:18 +0300

    added loaders into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +9,21 @@ module.exports = {
       {
         test: /.js?$/,
         loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
         exclude: /node_modules/
       }
     ]
@@ -23,6 +39,7 @@ module.exports = {
         from: __dirname + '/src/main.css',
         to: __dirname + '/build/main.css'
       }
-    ])
+    ]),
+    new ExtractTextPlugin('main.css')
   ]
 }
