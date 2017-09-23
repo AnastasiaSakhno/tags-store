8e4654e71790e113a5e03e828e069f8fccd6c380
Anastasiia Sakhno <ask@anahoret.com>
2017-09-23 18:27:33 +0300

    do not store tags in links table

--- src/sagas/links.js
+++ src/sagas/links.js
@@ -1,21 +1,24 @@
-import { takeLatest, takeEvery, put, call, all } from 'redux-saga/effects'
-import { snapshotToArray, getLinks, saveLink, getTags, saveTags } from '../utils/firebase'
+import { takeLatest, takeEvery, put, call } from 'redux-saga/effects'
+import { snapshotToArray, getLinks, saveLink, getTagsFor, saveTags } from '../utils/firebase'
 import actions from '../actions'
 import * as actionTypes from '../actions/types'
 
-export function *loadLinks() {
+export function* loadLinks() {
   const linksSnapshot = yield call(getLinks)
   const links = yield call(snapshotToArray, linksSnapshot)
 
   yield put(actions.links.loaded(links))
 }
 
-export function *addLink({ link }) {
-  yield call(saveLink, link)
+export function* addLink({ link }) {
+  // do not store tags in links table
+  yield call(saveLink, Object.assign({}, link, {
+    tags: null
+  }))
   yield call(saveTags, link)
 }
 
-export default function *watchLinks() {
+export default function* watchLinks() {
   yield [
     takeLatest(actionTypes.LOAD_LINKS, loadLinks),
     takeEvery(actionTypes.ADD_LINK, addLink)
