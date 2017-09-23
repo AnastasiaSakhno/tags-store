d1e22e0ac1d49b4584fda42bb522891402762a9b
Anastasiia Sakhno <ask@anahoret.com>
2017-09-23 18:29:21 +0300

    added icon button component

---
+++ src/components/IconButton.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class IconButton extends Component {
+
+  render() {
+    return (
+      <span className="iconbutton-box" onClick={ this.props.onSubmit }>
+        <i className={ 'fa ' + this.props.icon }/>
+      </span>
+    )
+  }
+}
+
+IconButton.propTypes = {
+  icon: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default IconButton
