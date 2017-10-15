688dd8f4103ad55859519cf1385616235990321c
Anastasiia Sakhno <ask@anahoret.com>
2017-10-15 19:37:12 +0300

    added session reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import links from './links'
 import filters from './filters'
+import { sessionReducer } from 'redux-react-session'
 import { combineReducers } from 'redux'
 
 const reducers = combineReducers({
-  links, filters
+  links, filters, session: sessionReducer
 })
 
 export default reducers
