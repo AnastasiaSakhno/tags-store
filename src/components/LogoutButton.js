c1602d3747c1f39a362225beb284f8c2f14fd921
Anastasiia Sakhno <ask@anahoret.com>
2017-10-15 19:37:39 +0300

    added logout button component

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { logout } from '../actions/auth'
+
+const LogoutButton = ({ logout }) => (
+  <button onClick={() => logout(history)}>Logout</button>
+)
+
+LogoutButton.propTypes = ({
+  logout: PropTypes.func.isRequired
+})
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => {
+    dispatch(logout())
+  }
+})
+
+export default connect(null, mapDispatchToProps)(withRouter(LogoutButton))
